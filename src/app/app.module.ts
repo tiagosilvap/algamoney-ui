2626b5af46bd8e4857e2b1251d9168783217abf2
Tiago Pereira <tiago.pereira@hotmart.com>
2023-08-29 19:21:23 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { TabViewModule } from 'primeng/tabview';
+import {AppComponent} from './app.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -10,9 +11,11 @@ import { TabViewModule } from 'primeng/tabview';
   ],
   imports: [
     BrowserModule,
-    TabViewModule
+    ButtonModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
